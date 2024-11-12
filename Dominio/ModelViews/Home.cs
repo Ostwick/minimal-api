7996d2bf88ab16bf758db8382db4131771e38f69
Ostwick <henriqueneji@gmail.com>
2024-11-12 20:03:48 -0300

    regras para autorização

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,7 @@
+namespace MinimalApi.Dominio.ModelViews;
+
+public struct Home
+{
+    public string Mensagem { get => "API de Veículos - Minimal API"; }
+    public string Doc { get => "/swagger"; }
+}
